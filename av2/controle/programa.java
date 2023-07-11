ff6e82b791e8c6bc9fbde5b7d6df3f092fec5d90
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2023-07-11 11:15:31 -0300

    Create programa.java

---
+++ av2/controle/programa.java
@@ -0,0 +1,11 @@
+package controle;
+
+import view.JanelaAluno;
+
+public class Programa {
+    public static void main(String[] args) {
+        JanelaAluno ja = new JanelaAluno();
+
+        ja.setVisible(true);
+    }
+}
